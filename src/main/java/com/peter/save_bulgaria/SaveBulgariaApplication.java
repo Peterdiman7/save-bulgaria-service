c0ad754732bbbe46578b45e17fc5e9b3d089fe77
Peter.Anastasov7 <peter.dim.an7@gmail.com>
2025-06-25 17:45:59 +0300

    initial commit

---
+++ src/main/java/com/peter/save_bulgaria/SaveBulgariaApplication.java
@@ -0,0 +1,13 @@
+package com.peter.save_bulgaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveBulgariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveBulgariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peter/save_bulgaria/SaveBulgariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peter.save_bulgaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveBulgariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
