642e80a25ba1bddfee0a34b3e4a7859021306d5c
Peter.Anastasov7 <peter.dim.an7@gmail.com>
2025-07-03 17:49:22 +0300

    Update PhotosRepository

--- src/main/java/com/peter/save_bulgaria/repository/PhotosRepository.java
+++ src/main/java/com/peter/save_bulgaria/repository/PhotosRepository.java
@@ -2,7 +2,25 @@ package com.peter.save_bulgaria.repository;
 
 import com.peter.save_bulgaria.model.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface PhotosRepository extends JpaRepository<Photo, Long> {}
+public interface PhotosRepository extends JpaRepository<Photo, Long> {
+
+    List<Photo> findByUserId(Long userId);
+
+    @Query("SELECT p FROM Photo p WHERE p.user.email = :email")
+    List<Photo> findByUserEmail(@Param("email") String email);
+
+    @Query("SELECT COUNT(p) FROM Photo p WHERE p.user.id = :userId")
+    Long countByUserId(@Param("userId") Long userId);
+
+    @Query("SELECT p FROM Photo p WHERE p.user.id = :userId ORDER BY p.id DESC")
+    List<Photo> findByUserIdOrderByIdDesc(@Param("userId") Long userId);
+
+    void deleteByUserId(Long userId);
+}
