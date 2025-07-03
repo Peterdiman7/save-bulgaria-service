1c8f4a050300ae0edfb3e2d504eaac2213bac7b5
Peter.Anastasov7 <peter.dim.an7@gmail.com>
2025-07-03 17:42:26 +0300

    Fixes

--- src/main/java/com/peter/save_bulgaria/controller/UserController.java
+++ src/main/java/com/peter/save_bulgaria/controller/UserController.java
@@ -3,6 +3,7 @@ package com.peter.save_bulgaria.controller;
 import com.peter.save_bulgaria.dtos.UserDTO;
 import com.peter.save_bulgaria.model.User;
 import com.peter.save_bulgaria.repository.UsersRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,9 @@ public class UserController {
     private UsersRepository usersRepository;
 
     @GetMapping
+    @Transactional
     public List<UserDTO> getAllUsers() {
+        // This already fetches users with their photos using the @Query annotation
         return usersRepository.findAllWithPhotos().stream()
                 .map(UserDTO::new)
                 .toList();
--- src/main/java/com/peter/save_bulgaria/model/User.java
+++ src/main/java/com/peter/save_bulgaria/model/User.java
@@ -18,7 +18,7 @@ public class User {
     private String email;
     private String name;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JsonManagedReference
     private List<Photo> photos;
 
